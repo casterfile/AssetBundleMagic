b8eb681e2a4f08c18cac85031eb1abbfc304885e
tonyMac <tonyMac@tonys-MacBook-Pro.local>
2019-12-02 13:48:37 +0800

    first commit

---
+++ Assets/_MyAsset/_Script/OutputTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OutputTest : MonoBehaviour {
+	int i = 0;
+	public Text OutputDAta;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		i = i + 1;
+		OutputDAta.text = "OutputDAta: "+i;
+	}
+}
